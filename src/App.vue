9a604f3daaf09e909e964b31ea8c0f1f9cebd6e7
Ryan Deba <ryandeba@gmail.com>
2020-11-05 19:16:17 -0600

    use vue-cli

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+  import Beer from './components/Beer.vue'
+
+  export default {
+    name: 'App',
+
+    components: {
+      "beer": Beer
+    },
+
+    data() {
+      return {
+        beers: new Array(99)
+          .fill("")
+          .map(() => { return {full: true}; })
+      };
+    },
+
+     computed: {
+      fullBeers: function() {
+        return this.beers.filter(b => b.full);
+      }
+    },
+
+    methods: {
+      emptyBeer: function(index) {
+        this.beers[index].full = false;
+      }
+    },
+  };
+</script>
+
+<template>
+  <div>
+    <h1>{{fullBeers.length}} Bottles of Beers on the Wall</h1>
+    <beer
+      v-for="(beer, i) in beers"
+      :key="i"
+      v-bind="beer"
+      v-on:remove="emptyBeer(i)"
+    ></beer>
+  </div>
+</template>
---
+++ src/components/Beer.vue
@@ -0,0 +1,16 @@
+<script>
+  export default {
+    props: {
+      full: {
+        type: Boolean,
+        default: true
+      }
+    },
+  }
+</script>
+
+<template>
+  <span v-on:click="$emit('remove')" v-show="full">
+    <i class="fas fa-beer" style="font-size:30px"></i>
+  </span>
+</template>
