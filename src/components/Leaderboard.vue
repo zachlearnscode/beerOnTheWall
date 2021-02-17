bf12909817b56d3ae20c8703fb9bc6e5342dfea8
Zach <zmcnulty980@gmail.com>
2021-02-17 10:49:09 -0600

    "Sorted Leaderboard"

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -3,25 +3,62 @@
   export default {
     props: ["beers"],
 
-    computed: {
-      countsByName() {
-        let result = {};
+    methods: {
+      sortBeers() {
+        let beersFiltered = this.beers.slice().filter(b => b.name);
+        
+        beersFiltered.sort((a, b) => {
+          let nameA = a.name.toUpperCase();
+          let nameB = b.name.toUpperCase();
 
-        this.beers
-          .filter(b => b.name)
-          .forEach(b => {
-            result[b.name] = result[b.name] || 0;
-            result[b.name]++;
-          });
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+
+          return 0;
+        });
+
+        return beersFiltered;
+      }
+    },
+
+    computed: {
+      leaderboard() {
+        let result = [];
+        let claimedBeers = this.sortBeers();
+
+        let drinker = claimedBeers[0].name, count = 1;
+        for (let b = 1; b < claimedBeers.length; b++) {
+          if (claimedBeers[b].name === drinker) {
+            count++;
+          } else {
+            result.push({drinker, count})
+            drinker = claimedBeers[b].name;
+            count = 1;
+          }
+        }
+
+        result.sort((a, b) => b.count - a.count)
 
         return result;
       }
     }
+
   };
 </script>
 
 <template>
   <div>
-    Leaderboard: {{ countsByName }}
+    <div>
+      Leaderboard:
+    </div>
+    <div v-for="(drinker, i) in leaderboard" :key="i">
+      <div>
+        <span>{{ drinker.drinker }}: </span><span>{{ drinker.count }}</span>
+      </div>
+    </div>
   </div>
 </template>
