32d871d889b97ad03392515d45fc1b9c9deec544
Zach <zmcnulty980@gmail.com>
2021-03-05 17:36:09 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:zachlearnscode/beerOnTheWall.git master:gh-pages
+git push -f https://github.com/zachlearnscode/beerOnTheWall.git master:gh-pages
 
 cd -
